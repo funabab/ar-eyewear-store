de47f62fc4e404705d1d7728fdc58c7cdf28174c
Abdulbassit Abdullahi <funabab@gmail.com>
2023-07-06 02:40:08 +0100

    feat: ar preview

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -2,7 +2,6 @@ const priceFormatter = Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'NGN',
   currencyDisplay: 'narrowSymbol',
-  trailingZeroDisplay: 'stripIfInteger',
-} as Intl.NumberFormatOptions)
+})
 
 export const formatPrice = (price: number) => priceFormatter.format(price)
